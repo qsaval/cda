c9edeb9841d5819a2ac8eced1c7adc70dd83f1f0
qsaval <115545576+qsaval@users.noreply.github.com>
2023-09-04 15:42:44 +0200

    modif

--- base_de_donnee/requete/requete_papyrus.sql
+++ base_de_donnee/requete/requete_papyrus.sql
@@ -1,3 +1,4 @@
+--requete
 --Quelles sont les commandes du fournisseur 09120 ?
 SELECT numcom 
 FROM entcom 
@@ -106,3 +107,28 @@ SELECT numfou, qtecde * priuni * 0.20
 FROM ligcom 
 JOIN entcom ON entcom.numcom = ligcom.numcom 
 GROUP BY numfou;
+
+
+--mise a jour
+--Application d'une augmentation de tarif de 4% pour le prix 1 et de 2% pour le prix2 pour le fournisseur 9180
+UPDATE produit
+SET prix1 = prix1 * 1.04,
+    prix2 = prix2 *1.02
+WHERE numfou = 9180;
+
+--Dans la table vente, mettre à jour le prix2 des articles dont le prix2 est null, en affectant a prix2 la valeur de prix1.
+UPDATE vente 
+set prix2 = 0, prix2 = prix1 
+
+--Mettre à jour le champ obscom en positionnant '*****' pour toutes les commandes dont le fournisseur a un indice de satisfaction <5
+UPDATE entcom
+join fournis on fournis.numfou = entcom.numfou
+set obscom = '*****'
+where fournis.satisf < 5;
+
+--Suppression du produit I110
+DELETE FROM produit WHERE codart = 'I110';
+DELETE FROM vente WHERE codart = 'I110';
+DELETE FROM ligcom WHERE codart = 'I110';
+
+--Suppression des entête de commande qui n'ont aucune ligne
