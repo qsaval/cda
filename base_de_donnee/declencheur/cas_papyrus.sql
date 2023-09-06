579acdf5539040a04d42b42cb0fe041ff4921c7c
qsaval <115545576+qsaval@users.noreply.github.com>
2023-09-06 09:22:08 +0200

    modif

---
+++ base_de_donnee/declencheur/cas_papyrus.sql
@@ -0,0 +1,9 @@
+CREATE TRIGGER alerte_produit AFTER UPDATE ON produit
+    FOR EACH ROW
+    BEGIN
+        DECLARE diff INT;
+        SET diff = NEW.stkale - NEW.stkphy;
+        IF diff > 0 THEN
+            UPDATE article_a_commande SET qte = diff-qte WHERE codart = NEW.codart;
+        END IF;
+    END;
