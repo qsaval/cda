8e577aa64dc6b95b8af9cd636f3abce1e056a148
qsaval <115545576+qsaval@users.noreply.github.com>
2023-11-13 15:04:18 +0100

    modif

---
+++ fil_rouge/Desktop/apiTest2/lireProduit.php
@@ -0,0 +1,24 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Methods: GET");
+header("Access-Control-Max-Age: 3600");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+
+$key = $_GET['key'];
+$jwt = "eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IlF1ZW50aW4gU2F2YWwiLCJpYXQiOjE1MTYyMzkwMjJ9";
+
+if($key == $jwt){
+    $id = $_GET['id'];
+
+    $db = new PDO('mysql:host=localhost;charset=utf8;dbname=vente_bd2', 'admin', 'Afpa1234');
+
+    $requete = $db->query ("SELECT d.id, b.titre, d.nb_commander, d.prix_commander, d.nb_commander * d.prix_commander as prix_total  from detail_commande d join bd b on b.id = d.bd_id where commande_id = " . $id);
+
+    $resultat = $requete->fetchAll(PDO::FETCH_OBJ);
+
+    echo json_encode($resultat);
+}
+else{
+    echo "la clé n'est pas la bonne";
+}
---
+++ fil_rouge/Desktop/apiTest2/lireUser.php
@@ -0,0 +1,22 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Methods: GET");
+header("Access-Control-Max-Age: 3600");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+
+$key = $_GET['key'];
+$jwt = "eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IlF1ZW50aW4gU2F2YWwiLCJpYXQiOjE1MTYyMzkwMjJ9";
+
+if($key == $jwt){
+    $db = new PDO('mysql:host=localhost;charset=utf8;dbname=vente_bd2', 'admin', 'Afpa1234');
+
+    $requete = $db->query ("SELECT id, nom_user, prenom_user from user");
+
+    $resultat = $requete->fetchAll(PDO::FETCH_OBJ);
+
+    echo json_encode($resultat);
+}
+else{
+    echo "la clé n'est pas la bonne";
+}
